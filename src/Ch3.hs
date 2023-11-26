78dd90fe97124ce6651f49b02d3c9c65e348c93a
goddesseyes <goddesseyes@protonmail.com>
2023-11-26 20:04:04 +0300

    Chapter 3 guards

--- src/Ch3.hs
+++ src/Ch3.hs
@@ -1,4 +1,4 @@
-module Ch3 (lucky, factorial, addVectors, first, second, third, pairs, head', tell) where
+module Ch3 (lucky, factorial, addVectors, first, second, third, pairs, head', tell, bmiTell, max', myCompare) where
 
 lucky :: Integer -> String
 lucky 7 = "Luck"
@@ -35,3 +35,21 @@ tell [] = "Empty list"
 tell [x] = "One elem in list " ++ show x
 tell [x, y] = "Two elem in list " ++ show x ++ " and " ++ show y
 tell (x : y : _) = "More then two elem in list. First two elems: " ++ show x ++ " and " ++ show y
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | weight / height ^ 2 <= 18.5 = "Тощий"
+  | weight / height ^ 2 <= 25.0 = "Норма"
+  | weight / height ^ 2 <= 30.0 = "Полнота"
+  | otherwise = "Лишний вес"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a >= b = a
+  | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+myCompare a b
+  | a == b = EQ
+  | a >= b = GT
+  | otherwise = LT
