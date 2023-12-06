85bcd453cc4accfda6425f13731d3a65bc53ffb9
goddesseyes <goddesseyes@protonmail.com>
2023-12-06 20:43:50 +0300

    Ch6 progress

--- src/Ch6.hs
+++ src/Ch6.hs
@@ -2,10 +2,12 @@ module Ch6
   ( numUniques,
     wordsNum,
     isIn,
+    encodeCesar,
   )
 where
 
-import Data.List (nub, group, sort, isPrefixOf, any, tails)
+import Data.List (nub, group, sort, isPrefixOf, tails)
+import Data.Char (ord, chr)
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -15,3 +17,9 @@ wordsNum = map(\ws -> (head ws, length ws)) . group . sort . words
 
 isIn :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+encodeCesar :: Int -> String -> String
+encodeCesar offset msg = map (\c -> chr $ ord c + offset) msg
+
+decodeCesar :: Int -> String -> String
+decodeCesar shift msg = encodeCesar (negate shift) msg
