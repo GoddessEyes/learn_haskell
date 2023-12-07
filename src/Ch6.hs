31517a70e2c929256b74ccff90e65f7b8b548365
goddesseyes <goddesseyes@protonmail.com>
2023-12-07 23:51:05 +0300

    Ch6 progress

--- src/Ch6.hs
+++ src/Ch6.hs
@@ -6,14 +6,14 @@ module Ch6
   )
 where
 
-import Data.List (nub, group, sort, isPrefixOf, tails)
-import Data.Char (ord, chr)
+import Data.Char (chr, digitToInt, ord)
+import Data.List (find, group, isPrefixOf, nub, sort, tails)
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
 wordsNum :: String -> [(String, Int)]
-wordsNum = map(\ws -> (head ws, length ws)) . group . sort . words
+wordsNum = map (\ws -> (head ws, length ws)) . group . sort . words
 
 isIn :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
@@ -23,3 +23,12 @@ encodeCesar offset msg = map (\c -> chr $ ord c + offset) msg
 
 decodeCesar :: Int -> String -> String
 decodeCesar shift msg = encodeCesar (negate shift) msg
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1 ..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1 ..]
