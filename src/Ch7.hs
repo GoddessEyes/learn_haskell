54e90a651e653d7da6a616186c51caae399f4066
goddesseyes <goddesseyes@protonmail.com>
2023-12-19 22:20:08 +0300

    Ch7 progress

--- src/Ch7.hs
+++ src/Ch7.hs
@@ -27,3 +27,16 @@ baseCircle r = Circle (Point 0 0) r
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
+
+data Car = Car { Company :: String
+               , model :: String
+               , year :: Int
+               } deriving (Show)
+
