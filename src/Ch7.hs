33cec626131e6436601c7760f0277e554d451036
goddesseyes <goddesseyes@protonmail.com>
2023-12-20 17:12:45 +0300

    Progress ch7

--- src/Ch7.hs
+++ src/Ch7.hs
@@ -35,8 +35,20 @@ data Person = Person { firstName :: String
                      , phoneNumber :: String
                      , flavor :: String } deriving (Show)
 
-data Car = Car { Company :: String
+data Car = Car { company :: String
                , model :: String
                , year :: Int
                } deriving (Show)
+
+
+data Vector a = Vector a a a deriving (Show)
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+scalarProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `scalarProd` (Vector l m n) = i * l + j * m + k * n
+
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i * m) (j * m) (k * m)
 
