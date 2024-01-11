516e1b83f1605f30f2582915152ef8c28daa1c60
goddesseyes <goddesseyes@protonmail.com>
2024-01-11 17:54:16 +0300

    1/2 Ch7 progress

--- src/Ch7.hs
+++ src/Ch7.hs
@@ -129,3 +129,26 @@ Empty ^++ ys = ys
 -- ghci> let a = 3 :-: 4 :-: Empty
 -- ghci> let b = 6 :-: 5 :-: Empty
 -- ghci> a ^++ b
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singletone :: a -> Tree a
+singletone x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singletone x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+nums = [8, 6, 4, 1, 7, 3, 5]
+
+numsTree = foldr treeInsert EmptyTree nums
