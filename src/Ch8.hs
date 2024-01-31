9beb4058d8003c5e276eae9242192182208d7971
goddesseyes <goddesseyes@protonmail.com>
2024-01-31 16:57:22 +0300

    Finish ch8 start ch9

--- src/Ch8.hs
+++ src/Ch8.hs
@@ -1,4 +1,16 @@
-module Ch8 (main) where
+module Ch8
+  ( main,
+    reverseWords,
+    reverseWords',
+    testWhenMonad,
+    testSequence,
+    testForever,
+    colorAssoc,
+  )
+where
+
+import Control.Monad
+import Data.Char
 
 main = putStrLn "Hello world"
 
@@ -12,3 +24,42 @@ reverseWords' = do
 
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
+
+testWhenMonad = do
+  input <- getLine
+  when (input == "test") $ do
+    putStrLn input
+
+-- without sequence
+-- ff = do
+--   a <- getLine
+--   b <- getLine
+--   c <- getLine
+--   print [a, b, c]
+--
+
+testSequence = do
+  rs <- sequence [getLine, getLine, getLine]
+  print rs
+
+-- mapM print [1,2,3] & mapM_ print [1,2,3]
+
+testForever = forever $ do
+  putStr "Input: "
+  l <- getLine
+  putStrLn $ map toUpper l
+
+colorAssoc = do
+  colors <-
+    forM
+      [1, 2, 3, 4]
+      ( \a -> do
+          putStrLn $
+            "Collor association for number"
+              ++ show a
+              ++ "?"
+          color <- getLine
+          return color
+      )
+  putStrLn "Colors association for 1,2,3 and 4: "
+  mapM putStrLn colors
