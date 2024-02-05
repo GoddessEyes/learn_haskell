ce960690fcd9013fcc13b712a7dbf1843ac7c302
goddesseyes <goddesseyes@protonmail.com>
2024-02-05 22:14:49 +0300

    Ch9 progress

--- src/Ch9.hs
+++ src/Ch9.hs
@@ -1,6 +1,18 @@
-main = do
-  contents <- getContents
-  putStrLn $ shortLinesOnly contents
+module Ch9 where
+
+-- main = do
+--   contents <- getContents
+--   putStrLn $ shortLinesOnly contents
+
+main = interact shortLinesOnly
 
 shortLinesOnly :: String -> String
 shortLinesOnly = unlines . filter (\line -> length line < 15) . lines
+
+respondPalindrome :: String -> String
+respondPalindrome =
+  unlines
+    . map (\xs -> if isPal xs then "Palindrome" else "Not palindrome")
+    . lines
+
+isPal xs = xs == reverse xs
