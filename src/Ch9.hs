86c8e4280ca53ad13ece70d6617c59980312840b
goddesseyes <goddesseyes@protonmail.com>
2024-02-06 21:52:13 +0300

    Ch9 progress

--- src/Ch9.hs
+++ src/Ch9.hs
@@ -1,10 +1,35 @@
-module Ch9 where
+module Ch9 (removeTodo) where
+
+import Data.List
+import System.Directory
+import System.IO
 
 -- main = do
 --   contents <- getContents
 --   putStrLn $ shortLinesOnly contents
 
 main = interact shortLinesOnly
+
+addTodo = do
+  todoItem <- getLine
+  appendFile "todo.txt" (todoItem ++ "\n")
+
+removeTodo = do
+  contents <- readFile "todo.txt"
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0 ..] todoTasks
+
+  putStrLn "Your tasks: "
+  mapM_ putStrLn numberedTasks
+  putStrLn "Remove number:"
+  numberString <- getLine
+  let number = read numberString
+      newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  hPutStr tempHandle newTodoItems
+  hClose tempHandle
+  removeFile "todo.txt"
+  renameFile tempName "todo.txt"
 
 shortLinesOnly :: String -> String
 shortLinesOnly = unlines . filter (\line -> length line < 15) . lines
