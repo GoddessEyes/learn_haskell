4d0ba8550c398a0eaa3446ada1c3866afd4e810e
goddesseyes <goddesseyes@protonmail.com>
2024-04-18 18:45:44 +0300

    Start Ch10

---
+++ src/Ch10/Ch10.hs
@@ -0,0 +1,13 @@
+module Ch10.Ch10 (solveRPN) where
+-- Решение задач в функциональном стиле
+
+solveRPN :: String -> Double
+solveRPN = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (x * y):ys
+        foldingFunction (x:y:ys) "+" = (x + y):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys
+        foldingFunction (x:y:ys) "/" = (y / x):ys
+        foldingFunction (x:y:ys) "^" = (y ** x):ys
+        foldingFunction (x:xs) "ln" = log x:xs
+        foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs numberString = read numberString:xs
