e7b72b38fbbe2ffeddb1218a9442cbdffe399274
goddesseyes <goddesseyes@protonmail.com>
2024-04-22 22:29:13 +0300

    Progress Ch11

--- src/Ch11/Ch11.hs
+++ src/Ch11/Ch11.hs
@@ -10,3 +10,14 @@ main = do
 -- fmap (replicate 3) Right 3
 -- fmap (replicate 3) Nothing
 -- fmap (replicate 3) Left "Whoops"
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust counter x) = CJust (counter + 1) (f x)
+
+--λ> fmap id (CJust 0 "ha-ha-ha") <- передали CJust 0
+--CJust 1 "ha-ha-ha" <- получили Cjust 1. Нарушение fmap id = id
+
+
