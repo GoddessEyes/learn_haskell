dc9e4376562c291db820b2db018e5d9c56a42131
goddesseyes <goddesseyes@protonmail.com>
2024-04-24 18:40:27 +0300

    Start Ch12

---
+++ src/Ch12/Ch12.hs
@@ -0,0 +1,34 @@
+module Ch12.Ch12 where
+
+
+
+import Data.Monoid
+
+
+newtype Pair b a = Pair {getPair :: (a, b)} deriving (Show)
+
+-- getPair $ fmap (*100) (Pair (2, 3))
+instance Functor (Pair c) where
+  fmap f (Pair (x, y)) = Pair (f x, y)
+
+
+--data CoolBool = CoolBool {getCoolBool :: Bool}
+newtype CoolBool = CoolBool {getCoolBool :: Bool}
+
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "Hello"
+
+-- Monoids examples:
+--[1, 2, 3] `mappend` [4, 5, 6]
+--"one two" `mappend` "three"
+--mconcat [[1,2], [3, 4]]
+--getAny $ Any True `mappend` Any False
+--getAll $ All True `mappend` All False
+
+productEx :: Integer
+productEx =
+  getProduct $ Product 3 `mappend` Product 9
+
+sumEx :: Integer
+sumEx =
+  getSum $ Sum 2 `mappend` Sum 9
