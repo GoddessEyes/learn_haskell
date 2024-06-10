6f84f15822e905eb5b3c3c4d53c2288f4ea19072
goddesseyes <goddesseyes@protonmail.com>
2024-06-10 13:22:22 +0300

    Progress Ch14

--- src/Ch14/Ch14.hs
+++ src/Ch14/Ch14.hs
@@ -1,5 +1,6 @@
-module Ch14.Ch14 (isBigGang, addDrink, applyLog) where
+module Ch14.Ch14 (isBigGang, addDrink, applyLog, multWithLog, multWithLog', gcd') where
 
+import Control.Monad.Writer
 import Data.Monoid
 
 isBigGang :: Int -> (Bool, String)
@@ -18,3 +19,32 @@ addDrink "Meat" = ("Whiskey", Sum 99)
 addDrink _ = ("Beer", Sum 30)
 
 -- ("Beans", Sum 10) `applyLog` addDrink
+-- runWriter (return 3 :: Writer Sum Int Int)
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  return (a * b)
+
+multWithLog' :: Writer [String] Int
+multWithLog' = do
+  a <- logNumber 3
+  b <- logNumber 5
+  tell ["Got it"]
+  return (a * b)
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+  | b == 0 = do
+    tell ["Finish: " ++ show a]
+    return a
+  | otherwise = do
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    gcd' b (a `mod` b)
+
+-- fst $ runWriter (gcd' 3 8)
+-- mapM_ putStrLn $ snd $ runWriter (gcd' 3 8)
